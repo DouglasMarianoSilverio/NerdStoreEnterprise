b2a2ca9c5c643f6c415afaa7d8d452a740aeff86
Douglas Mariano Silverio <douglas07@gmail.com>
2021-01-21 18:42:24 -0300

    COmportamentos basicos da APi de catalogo.

--- src/services/NSE.Catalogo.API/Models/Produto.cs
+++ src/services/NSE.Catalogo.API/Models/Produto.cs
@@ -9,7 +9,7 @@ namespace NSE.Catalogo.API.Models
         public string Descricao { get; set; }
         public bool Ativo { get; set; }
         public decimal Valor { get; set; }
-        public DateTime DataCadastrado { get; set; }
+        public DateTime DataCadastro { get; set; }
         public string Imagem { get; set; }
         public int QuantidadeEstoque { get; set; }
     }
